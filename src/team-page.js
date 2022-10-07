0b953b9103967c91ef4224dd8628d0661b0b1f2a
Bhavika Mevada <109460560+bmevada@users.noreply.github.com>
2022-10-07 20:40:01 +1100

    css and html style errors fixed

--- src/team-page.js
+++ src/team-page.js
@@ -91,9 +91,9 @@ module.exports = (team) => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <title>Team Profile Generator</title>
-      <link rel="stylesheet" href="href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+      <link rel="stylesheet" href=https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link rel="stylesheet" href=".dist/styles.css">
+      <link rel="stylesheet" href=".styles.css">
   </head>
   <body>
       <div class="container-fluid">
